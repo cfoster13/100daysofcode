4b52b191c425905fdc5a251f4474e26e4129f04a
cfoster13 <chayefoster@icloud.com>
2024-11-18 19:39:26 +0000

    Added a high score function to the snake game, which reads a data.txt file and updates it when the player scores a new high score.

--- week4/Snake_Game/scoreboard.py
+++ week4/Snake_Game/scoreboard.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 
-
+with open("week4/Snake_Game/data.txt") as file:
+    high_score_amount = int(file.read())
+    
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -8,17 +10,30 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.score = 0 # Starting score of 0
+        self.high_score = high_score_amount
         self.penup()
         self.goto(0, 270)
-        self.write(arg=f"Score : {self.score}", align="center", font=("Arial", 22, "normal"))
+        self.write(arg=f"Score : {self.score} High score: {self.high_score}", align="center", font=("Arial", 22, "normal"))
         
         
 
     def update_score(self):
         self.clear()
-        self.score += 1 # Add one to score
-        self.write(arg=f"Score : {self.score}", align="center", font=("Arial", 22, "normal"))
+        self.write(arg=f"Score : {self.score} High score: {self.high_score}", align="center", font=("Arial", 22, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align="center", font=("Arial", 22, "normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg="GAME OVER", align="center", font=("Arial", 22, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("week4/Snake_Game/data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0 # Reset the score
+        self.update_score()
+
+    def increase_score(self):
+        self.score += 1 # Add one to score
+        self.update_score()
+
--- week4/Snake_Game/snake.py
+++ week4/Snake_Game/snake.py
@@ -26,6 +26,13 @@ class Snake:
         snake.goto(position)
         self.snakes.append(snake)
 
+    def reset(self):
+        for seg in self.snakes:
+            seg.goto(1000, 1000)
+        self.snakes.clear()
+        self.create_snake()
+        self.head = self.snakes[0]
+
     def extend(self):
         self.add_segment(self.snakes[-1].position())
 
